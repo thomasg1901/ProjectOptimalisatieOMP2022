8d7e68fe35e52d134adc424fb04a56f34358a69f
arijnborzo <arijn.b@hotmail.com>
2022-11-09 12:34:06 +0100

    start

--- OMP/src/Main.java
+++ OMP/src/Main.java
@@ -1,6 +1,39 @@
+import org.json.*;
+import com.google.gson.*;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        
+        try {
+            // create Gson instance
+            Gson gson = new Gson();
+
+            // create a reader
+
+            Reader reader = Files.newBufferedReader(Paths.get("/Users/arijnborzo/ProjectOptimalisatieOMP2022/OMP/file.json"));
+
+            // convert JSON file to map
+            Map<?, ?> map = gson.fromJson(reader, Map.class);
+
+            // print map entries
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + "=" + entry.getValue());
+            }
+
+            // close reader
+            reader.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
+
+
 }
+
+
+
