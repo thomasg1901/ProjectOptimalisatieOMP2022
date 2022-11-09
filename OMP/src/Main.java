ca6140c114c0ad6dd5bd38674911ac8367ec405b
jefjacobs00 <jacobsjef16@gmail.com>
2022-11-09 12:18:42 +0100

    resource files

--- OMP/src/Main.java
+++ OMP/src/Main.java
@@ -1,6 +1,9 @@
+import org.json.*;
+import com.google.gson.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        
+        Gson gson = new Gson();
     }
 }
