2be8bed397d2ed429de34a47b48bf3107906cdc1
jefjacobs00 <jacobsjef16@gmail.com>
2022-11-14 13:43:54 +0100

    jsonreader for jobs

--- OMP/src/JsonReader.java
+++ OMP/src/JsonReader.java
@@ -1,2 +1,50 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonReader {
+    public JsonReader(){
+
+    }
+
+    public ArrayList<Job> getJoblistFromFile(String file) throws IOException {
+        ArrayList<Job> jobs = new ArrayList<>();
+        String resourceName = "./file.json";
+        String is = new String(Files.readAllBytes(Paths.get(resourceName)));
+        JSONTokener tokener = new JSONTokener(is);
+        JSONObject object = new JSONObject(tokener);
+        JSONArray jsonjobs = object.getJSONArray("jobs");
+        JSONArray jsonsetup = object.getJSONArray("setups");
+
+        for (int i=0; i<jsonjobs.length();i++){
+            JSONObject data = jsonjobs.getJSONObject(i);
+
+            jobs.add(new Job((int)data.get("id"),
+                    (int)data.get("duration"),
+                    (int)data.get("release_date"),
+                    ((BigDecimal) data.get("earliness_penalty")).doubleValue(),
+                    ((BigDecimal) data.get("rejection_penalty")).doubleValue(),
+                    convertJsonArray(jsonsetup.getJSONArray((int)data.get("id")))));
+        }
+
+        return jobs;
+    }
+
+    private static int[] convertJsonArray(JSONArray array){
+        int[] result = new int[array.length()];
+
+        for (int i = 0; i < array.length();i++){
+            result[i] = Integer.parseInt(""+array.get(i));
+        }
+
+        return result;
+    }
+
 }
--- OMP/src/Main.java
+++ OMP/src/Main.java
@@ -11,35 +11,10 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ArrayList<Job> jobs = new ArrayList<>();
-        String resourceName = "./file.json";
-        String is = new String(Files.readAllBytes(Paths.get(resourceName)));
-        JSONTokener tokener = new JSONTokener(is);
-        JSONObject object = new JSONObject(tokener);
-        JSONArray jsonjobs = object.getJSONArray("jobs");
-        JSONArray jsonsetup = object.getJSONArray("setups");
 
-        for (int i=0; i<jsonjobs.length();i++){
-            JSONObject data = jsonjobs.getJSONObject(i);
-
-            jobs.add(new Job((int)data.get("id"),
-                    (int)data.get("duration"),
-                    (int)data.get("release_date"),
-                    ((BigDecimal) data.get("earliness_penalty")).doubleValue(),
-                    ((BigDecimal) data.get("rejection_penalty")).doubleValue(),
-                    convertJsonArray(jsonsetup.getJSONArray((int)data.get("id")))));
-        }
     }
 
-    private static int[] convertJsonArray(JSONArray array){
-        int[] result = new int[array.length()];
 
-        for (int i = 0; i < array.length();i++){
-            result[i] = Integer.parseInt(""+array.get(i));
-        }
-
-        return result;
-    }
 
 
 }
