6419fdb626935ba1512045a9fc6b60729174a781
Jef <Jacobsjef16@gmail.com>
2022-11-21 20:04:01 +0100

    added python script to test all files

---
+++ scratch.py
@@ -0,0 +1,23 @@
+import os
+from subprocess import Popen, PIPE
+
+inputFolder = r'OMP/src/resources'
+solutionFolder = r'OMP/output'
+
+script = r'OMP/validator.py'
+print(os.path)
+inputFiles = os.listdir(inputFolder)
+solutionFiles = os.listdir(solutionFolder)
+
+for file in inputFiles:
+    if solutionFiles.__contains__('sol-'+file):
+        cmd = f'python {script} -i {inputFolder}/{file} -s {solutionFolder}/sol-{file}'
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, close_fds=True)
+        (output, err) = p.communicate()
+        output = output.decode("utf-8")
+
+        print(file)
+        print(output)
+        print(err)
+
+
